870d3d1555e02a0067df9c812336c2415463ca1f
Eoin Chedzey <eoinisnow@gmail.com>
2023-04-07 15:52:46 +0100

    averageDroneElevation is calculated during post-process and appended to yaml file, removes any vertices with -1.0 ie. fault reading from lidar sensor

--- scripts/sensorFusionToPoints.py
+++ scripts/sensorFusionToPoints.py
@@ -1,6 +1,8 @@
 import os
 import sys 
 from datetime import datetime
+import yaml
+
 
 # datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
 # os.path.exists("file.txt") # Or folder, will return true or false
@@ -9,7 +11,9 @@ def sensorFusionToPoints(fileName):
     print('called sensorFusionToPoints()')
 
     # check that file does not exist in processedData folder
-    if(checkFileExists("processedData/1d_" + fileName)):
+    if(checkFileExists("processedData/1d_0_" + fileName)):
+        print("File already exists in processedData folder")
+    elif(checkFileExists("processedData/1d_1_" + fileName)):
         print("File already exists in processedData folder")
         return
     else:
@@ -18,22 +22,49 @@ def sensorFusionToPoints(fileName):
         open("processedData/1d_" + fileName, 'w').close() 
 
     with open("inputData/" + fileName) as file_in:
+        averageElevation = 0
+        averageCounter = 0
         lines = []
         points = []
         for line in file_in:
             #lines.append(line)
             #print(type(line))
             pointsStr = line.split(",")
+
+            averageElevation += float(pointsStr[2]) 
+            averageCounter += 1
             # print(pointsStr)
-            points.append([float(pointsStr[0]), float(pointsStr[1]), float(pointsStr[2]) - (float(pointsStr[3]) * 0.01) ])
+            points.append([float(pointsStr[0]), float(pointsStr[1]), float(pointsStr[2]) - (float(pointsStr[3]) * 0.01) ]) # x, z, y
             points.append([float(pointsStr[0]), float(pointsStr[1]), float(pointsStr[2]) + (float(pointsStr[4]) * 0.01) ])
 
             with open("processedData/1d_0_" + fileName, "a") as file_out:
                 #verticeString = 
-                file_out.write(pointsStr[0] + " , " +  str(float(pointsStr[2]) - (float(pointsStr[3]) * 0.01) ) + " , " + pointsStr[1] + "\n" )
+                if(float(pointsStr[3]) != -1.0):
+                    file_out.write(pointsStr[0] + " , " +  str(float(pointsStr[2]) - (float(pointsStr[3]) * 0.01) ) + " , " + pointsStr[1] + "\n" )
             with open("processedData/1d_1_" + fileName, "a") as file_out:
-                file_out.write(pointsStr[0] + " , " +  str(float(pointsStr[2]) + (float(pointsStr[4]) * 0.01) ) + " , " + pointsStr[1] + "\n" )
-                
+                if(float(pointsStr[4]) != -1.0):
+                    file_out.write(pointsStr[0] + " , " +  str(float(pointsStr[2]) + (float(pointsStr[4]) * 0.01) ) + " , " + pointsStr[1] + "\n" )
+
+        # open yaml file and append data to it 
+        fileName = fileName.split(".")[0] # remove .txt from filename
+
+        averageElevation = averageElevation / averageCounter
+
+        # open corresponding 2d processed file
+
+
+        with open("inputData/" + fileName + '.yaml','r') as yamlfile:
+            cur_yaml = yaml.safe_load(yamlfile) # Note the safe_load
+            #cur_yaml['bugs_tree'].update(new_yaml_data_dict)
+            cur_yaml['averageElevation'] = averageElevation
+
+        if cur_yaml:
+            with open("inputData/" + fileName + '.yaml','w') as yamlfile:
+                yaml.safe_dump(cur_yaml, yamlfile) # Also note the safe_dump
+            print(open("inputData/" + fileName + '.yaml').read())
+
+
+# print(open('names.yaml').read())
 
 def checkFileExists(fileNamePath):
     if os.path.exists(fileNamePath):
